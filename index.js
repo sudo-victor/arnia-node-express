6e5f4149345584cfba921759546e7dd70c62703a
sudo-victor <56266573+sudo-victor@users.noreply.github.com>
2022-10-11 20:27:04 -0300

    remove maps functions

--- index.js
+++ index.js
@@ -6,15 +6,7 @@ function createUser(name, age, bancoDados){
   // utilizar factory function para criar um usuário
 }
 
-function createUserMap(name, age, bancoDados){
-  // utilizar factory function para criar um usuário
-}
-
 function findOneUser (index, bancoDados) {
-  // utilizar função de iteração para encontrar o usuário desejado
-}
-
-function findOneUserMap(index, bancoDados) {
   // utilizar função de iteração para encontrar o usuário desejado
 }
 
@@ -23,5 +15,9 @@ function getAllUser(bancoDados) {
 }
 
 function updateUser(id, name, age, bancoDados) {
+  // sem dica ;)
+}
+
+function usersCount(bancoDados) {
   // sem dica ;)
 }
