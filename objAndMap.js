60f34f9108bc1a14bb771c41503c39d529f502f1
sudo-victor <56266573+sudo-victor@users.noreply.github.com>
2022-10-11 20:54:50 -0300

    add consoles

--- objAndMap.js
+++ objAndMap.js
@@ -21,3 +21,15 @@ function updateUser(id, name, age, bancoDados) {
 function usersCount(bancoDados) {
   // sem dica ;)
 }
+
+createUser('Lucas Perdigão', '40', bancoDadosObj)
+createUser('Ricardo Borges', '22', bancoDadosObj)
+createUser('Lucas Perdigão', '18', bancoDadosObj)
+createUser('Mauro Luiz', '70', bancoDadosObj)
+createUser('Alan Almeida', '25', bancoDadosObj)
+
+updateUser(1, 'Rafael Vinicius', '30', bancoDadosObj)
+
+console.log(findOneUser(1, bancoDadosObj))
+console.log(getAllUser(bancoDadosObj))
+console.log(usersCount(bancoDadosObj))
