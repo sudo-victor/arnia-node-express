05d91036cea80174464e93fa65239a35f007f64a
sudo-victor <56266573+sudo-victor@users.noreply.github.com>
2022-10-11 20:25:53 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const bancoDadosObj = new Array()
+
+// Crie uma Factory Function para gerar um usuário
+
+function createUser(name, age, bancoDados){
+  // utilizar factory function para criar um usuário
+}
+
+function createUserMap(name, age, bancoDados){
+  // utilizar factory function para criar um usuário
+}
+
+function findOneUser (index, bancoDados) {
+  // utilizar função de iteração para encontrar o usuário desejado
+}
+
+function findOneUserMap(index, bancoDados) {
+  // utilizar função de iteração para encontrar o usuário desejado
+}
+
+function getAllUser(bancoDados) {
+  // não é necessário dica ;)
+}
+
+function updateUser(id, name, age, bancoDados) {
+  // sem dica ;)
+}
