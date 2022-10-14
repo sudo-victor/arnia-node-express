94dc0fb78a01bd92822665c1efdc308ab6c8bdd4
Victor de Souto <victor.souto@hotmail.com>
2022-10-14 09:42:58 -0300

    fix: lesson

--- functions.js
+++ functions.js
@@ -9,52 +9,77 @@ function carFactory({ name, year, color, price }) {
         owner: null
     }
 }
-
+[1,2,3].filter((qualquerPalavra) => )
 // Dica: para manipular o banco de dados devem utilizar o THIS
 
 function repository(database) {
     return {
         db: database,
 
-        // deve ser passado os PARÂMETROS ({ name, year, color, price } à função, GERAR um carro(com a FACTORY FUNCTION) e PERSISTIR no banco de dados
-        create: this,
+        create: function(param) {
+            return this.bg.push(carFactory(param))
+        },
 
-        // deve ser capaz de RETORNAR todos os dados PERSISTIDOS
-        findAll: () => {
+        findAll: function() {
             return this.db;
         },
 
-        // deve ser passado o PARÂMETRO (year) e retornar todos os dados que tenham o mesmo ano
-        findAllByYear: null,
+        findAllByYear: function(year) {
+            return this.db.filter((car) => car.year === year)
+        },
 
-        // deve ser capaz de RETORNAR todos os carros vendidos
-        findAllSold: null,
+        findAllSold: function() {
+            return this.db.filter((car) => car.sold)
+        },
 
-        // deve ser passado o PARÂMETRO (name) e RETORNAR o objeto que tenham o mesmo nome
-        findOneByName: null,
+        fJBindOneByName: function(name) {
+            return this.db.find((car) => car.name === name)
+        },
 
-        // deve ser passado os PARÂMETROS (carName, ownerName) e atualizar os atributos (name, owner, sold) de um objeto no banco de dados
-        updateOwner: null,
+        updateOwner: function(carName, ownerName) {
+            const car = this.db.find((c) => c.name === carName)
+            this.db.foreach((car) => {
+                car.owner = 
+            })
+            if (car) {
+                car.owner = ownerName;
+                car = true;
+            }
+        },
 
     }
 }
 
 const cars = []
 const carRepository = repository(cars)
+console.log(carRepository)
+// carRepository.create({ name: 'Fiat Uno', year: 2020, color: 'gray', price: 15000 })
+// carRepository.create({ name: 'Drako GTE', year: 2019, color: 'red', price: 6197115 })
+// carRepository.create({ name: 'Gordon Murray T.33', year: 2020, color: 'black', price: 8779247 })
+// carRepository.create({ name: 'Gordon Murray T.33', year: 2020, color: 'black', price: 8779247 })
 
-carRepository.create({ name: 'Fiat Uno', year: 2020, color: 'gray', price: 15000 })
-carRepository.create({ name: 'Drako GTE', year: 2019, color: 'red', price: 6197115 })
-carRepository.create({ name: 'Gordon Murray T.33', year: 2020, color: 'black', price: 8779247 })
-carRepository.create({ name: 'Gordon Murray T.33', year: 2020, color: 'black', price: 8779247 })
+// carRepository.updateOwner('Fiat Uno', 'Pedro Souza')
+// carRepository.updateOwner('Drako GTE', 'Rafael Vinicios')
+// carRepository.updateOwner('Gordon Murray T.33', 'Lucas Perdigão')
 
-carRepository.updateOwner('Fiat Uno', 'Pedro Souza')
-carRepository.updateOwner('Drako GTE', 'Rafael Vinicios')
-carRepository.updateOwner('Gordon Murray T.33', 'Lucas Perdigão')
+// console.log(userRepository.findOneByName('Fiat Uno'))
+// console.log('\n\n\n')
+// console.log(userRepository.findAllByYear(2020))
+// console.log('\n\n\n')
+// console.log(userRepository.findAllSold())
+// console.log('\n\n\n')
+// console.log(userRepository.findAll())
 
-console.log(userRepository.findOneByName('Fiat Uno'))
-console.log('\n\n\n')
-console.log(userRepository.findAllByYear(2020))
-console.log('\n\n\n')
-console.log(userRepository.findAllSold())
-console.log('\n\n\n')
-console.log(userRepository.findAll())
+
+const cachorro = {
+    nome: 'Rex',
+    raca: '',
+    latir: function () {
+        return this.nome + ' latiu'
+    },
+    qualRaca: function () {
+        console.log(this.raca)
+    }
+}
+
+cachorro.latir()
---
+++ teste.js
@@ -0,0 +1,13 @@
+function () {
+    const cars = [
+        { name: 'Fiat' },
+        { name: 'Celta' },
+    ]
+
+    cars.map((car) => {
+        car.owner = 'teste'
+    })
+
+    const novaLista = []
+    for
+}
