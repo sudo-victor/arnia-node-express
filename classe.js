296341d9528716b5802c473fa2fe7de3c632b696
Victor de Souto <victor.souto@hotmail.com>
2022-10-17 20:20:47 -0300

    chore: add lesson to classe

---
+++ classe.js
@@ -0,0 +1,26 @@
+/**
+ * Crie uma classe chamada RealConverter. O objetivo dela é agrupar funções que convertem o Real para outras moedas.
+ * Ela deverá conter 4 métodos, sendo eles: toDollar, toEuro, toBitcoin e exchange
+ * o método exchange será o responsavel por chamar os métodos de conversão, para isso,
+ * ele deve receber os PARÂMETROS: value e coin('usd', 'eur', 'btc').
+ * deve ser utilizado alguma estrutura condicional(if, switch, outro) para verificar o tipo da coin('usd', 'eur', 'btc') e executar o método
+ * desejado.
+ * Cada método de conversão deve receber o PARÂMETRO: value. e retornar o valor calculado e formatado.
+ * 1real = 5.26 dolar
+ * 1real = 5.18 euro
+ * 1real = 103326.22 bitcoin
+ * 
+ * dica do método exchange
+ * exchange(value, coin) {
+ *  if (coin === 'usd') {
+ *      return toDollar()
+ *  }
+ * }
+ */
+
+
+
+// const realConverter = new RealConverter();
+// console.log(realConverter.exchange(10, 'btc'))
+// console.log(realConverter.exchange(10, 'usd'))
+// console.log(realConverter.exchange(10, 'eur'))
--- recap-function-obj.js
+++ recap-function-obj.js
@@ -1,17 +1,16 @@
 const agendamentos = [
-    { nomePaciente: 'Alan Almeida', data: '14/10/2022', horaInicio: '20:15', horaFim: '20:45'},
-    { nomePaciente: 'Pedro Sousa', data: '14/10/2022', horaInicio: '12:00', horaFim: '12:30'},
-    { nomePaciente: 'Rafael Vinicius', data: '14/10/2022', horaInicio: '12:20', horaFim: '13:00'},
-    { nomePaciente: 'Rangel Tomazilli', data: '14/10/2022', horaInicio: '15:25', horaFim: '15:55'},
+    { paciente: 'Alan Almeida', data: '14/10/2022', horaInicio: '20:15', horaFim: '20:45' },
+    { paciente: 'Pedro Sousa', data: '14/10/2022', horaInicio: '12:00', horaFim: '12:30' },
+    { paciente: 'Rafael Vinicius', data: '14/10/2022', horaInicio: '12:20', horaFim: '13:00' },
+    { paciente: 'Rangel Tomazilli', data: '14/10/2022', horaInicio: '15:25', horaFim: '15:55' },
 ]
 
 // Crie uma factory function para gerar um agendamento
 // Parâmetros: nome, sobrenome, data, horaInicio
-// Retorno: { paciente, data, horaInicio, horaFim}
+// Retorno: { paciente, data, horaInicio, horaFim }
 // Obs: um agendamento têm 30 minutos de duração
 // Plus: criar uma classe que represente um Agendamento, com o construtor
-// se comportando igual a factory function 
-
+// se comportando igual a factory function
 
 // Crie uma função para verificar se já existe algum agendamento
 // com a data e o horário de inicio passados pelo parâmetro
@@ -24,3 +23,13 @@ const agendamentos = [
 // Parâmetro: valor
 // Retorno: { paraDolar, paraEuro }
 // Obs: 1 dolar === 5,30 reais   e   1 euro === 5,16 real
+
+function converter(valor) {
+    return {
+        paraDolar: () => valor * 5.30,
+        paraEuro: () => valor * 5.16,
+    }
+}
+
+console.log(converter(5).paraDolar())
+console.log(converter(2).paraEuro())
