e09e3929153024e0c031b0f57cf6d8ea616ae13e
Victor de Souto <victor.souto@hotmail.com>
2022-10-21 22:40:23 -0300

    solve terminal lesson

--- terminal/index.js
+++ terminal/index.js
@@ -5,29 +5,94 @@ import fs from 'fs';
 import path from 'path';
 import {fileURLToPath} from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const resultPath = path.join(__dirname,  'result.txt')
+const lembretesPath = path.join(__dirname,  'lembretes.txt')
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// const resultPath = path.join(__dirname,  'result.json')
+// const questions = [
+//     {
+//       name: "lembrete",
+//       type: "list",
+//       message: "Qual açao deseja fazer?",
+//       choices: ["Salvar", "Ler"],
+//     },
+// ]
 
-const questions = [
-    {
-      name: "pergunta_1",
-      type: "input",
-      message: "Digite algo",
-    },
-    {
-      name: "pizza",
+
+// const questions2 = [
+//     {
+//       name: "nome",
+//       type: "input",
+//       message: "Qual o seu nome?",
+//     },
+//     {
+//       name: "idade",
+//       type: "input",
+//       message: "Qual o sua idade?",
+//     },
+// ]
+
+// inquirer
+//     .prompt(questions2)
+//     .then(({ nome, idade }) => {
+//       fs.writeFileSync(resultPath, `O ${nome} tem ${idade} anos`)
+//     })
+//     .catch((error) => {
+//       if (error.isTtyError) {
+//         // Prompt couldn't be rendered in the current environment
+//         console.log('n deu pra rodar')
+//         const result1 = path.join()
+//       } else {
+//         // Something else went wrong
+//         console.log(error)
+//       }
+//     });
+
+
+const question3 = [
+      {
+      name: "lembrete",
       type: "list",
-      message: "Qual pizza você prefere?",
-      choices: ["Mussarela", "Portuguesa", "Camarão"],
+      message: "Qual açao deseja fazer?",
+      choices: ["Salvar", "Ler"],
     },
 ]
 
+const question4 = [
+  {
+    name: "aviao",
+    type: "input",
+    message: "O que vc quer ser lembrado?",
+  },
+]
+
 inquirer
-    .prompt(questions)
-    .then((answers) => {
-        console.log(answers)
+    .prompt(question3)
+    .then((anwser) => {
+      if(anwser.lembrete === "Salvar") {
+        inquirer
+          .prompt(question4)
+          .then((props) => {
+            const lembretesSalvos = fs.readFileSync(lembretesPath, "utf8");
+            const result = lembretesSalvos + "\n" + props.aviao
+            fs.writeFileSync(lembretesPath, result)
+          })
+          .catch((error) => {
+            if (error.isTtyError) {
+              // Prompt couldn't be rendered in the current environment
+              console.log('n deu pra rodar')
+            } else {
+              // Something else went wrong
+              console.log(error)
+            }
+          });
+      }
+      if (anwser.lembrete === "Ler") {
+        const lembretes = fs.readFileSync(lembretesPath, "utf8");
+        console.log(lembretes);
+        // Console do conteudo do arquivo lembretes
+      }
     })
     .catch((error) => {
       if (error.isTtyError) {
@@ -44,10 +109,13 @@ inquirer
 // O {nome} tem {idade} anos
 
 
+
 // EXERCÍCIO 2
 // Perqunte se o usuário deseja salvar ou ler os lembretes
 // Salvar: deve ser peguntado qual lembrete ele que salvar e adicionar a resposta no final do arquivo lembrete
 // Ler: deve ser retornado no console a listagem de lembretes
+
+
 
 // Consultas
 // https://github.com/SBoudrias/Inquirer.js
