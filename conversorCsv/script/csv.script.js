8fdfaab8b8fe2f261d35fcc7bbf2f0210c4c90b3
Victor de Souto <victor.souto@hotmail.com>
2022-10-24 19:41:17 -0300

    chore: update doc

--- conversorCsv/script/csv.script.js
+++ conversorCsv/script/csv.script.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
-// A função csvDataFormatted deverá formatar o conteudo do arquivo pokemons.csv
+// A função csvDataFormatted deverá formatar o conteudo do arquivo data/pokemons.csv
 // e retorna um array de objetos:
 // {
 //     name: 'Cloyster',
-//     type: 'Water Ice',
+//     type: 'Water Ice', junçao do type 1 e type 2
 //     total: '525',
 //     hp: '50',
 //     attack: '95',
