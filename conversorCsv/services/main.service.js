ca76e4c802295fff2693fce81c1d46470a7247c4
Victor de Souto <victor.souto@hotmail.com>
2022-10-24 20:40:31 -0300

    remove solves

--- conversorCsv/services/main.service.js
+++ conversorCsv/services/main.service.js
@@ -1,34 +1,19 @@
+
+// A classe MongoRepository deverá receber o parâmetro mongoRepository no construtor e 3 métodos
+// constructor:
+//          Parâmetro: mongoRepository(instancia do repositório do mongo)
+// getAll:
+//         Parâmetro: collection(string)
+//         Retorno: array
+// getOne:
+//         Parâmetro: collection(string), id(string)
+//         Retorno: object
+// create:
+//         Parâmetro: collection(string), data(object)
+//         Retorno: 
+
+
 class MainService {
-    constructor(mongoRepository) {
-        this.mongoRepository = mongoRepository;
-    }
-
-    async getAll(collection) {
-        try {
-            const result = await this.mongoRepository.getAll(collection);
-            return result;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async getOne(collection, id) {
-        try {
-            const result = await this.mongoRepository.getOne(collection, id);
-            return result;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async create(collection, data) {
-        try {
-            const result = await this.mongoRepository.create(collection, data);
-            return result;
-        } catch (error) {
-            console.log(error);
-        }
-    }
 }
 
 module.exports = { MainService };
---
+++ tests/repository.js
@@ -0,0 +1,2 @@
+// Crie uma funçao que recebe dois parametros, e retorna a soma dos dois
+// Crie uma funçao que recebe um array de números, e retorna a soma dos dois
