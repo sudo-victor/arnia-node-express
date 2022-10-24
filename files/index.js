44d2bac00f46da3207ec60ddef10d76f9a372b00
Victor de Souto <victor.souto@hotmail.com>
2022-10-24 09:00:19 -0300

    chore: solve files project

--- files/index.js
+++ files/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const filePath = path.join(__dirname, "./arnia.csv");
 const resultPath = path.join(__dirname, "./result.json");
+const result2Path = path.join(__dirname, "./result2.json");
 
 const csvText = fs.readFileSync(filePath, "utf8")
 
@@ -10,12 +11,19 @@ const dataSeparated = csvText.replace(/(\r\n)|\r|\n/g, '\n').split(/\n+/g)
 
 fs.writeFileSync( resultPath, JSON.stringify({ message: "Hello World 2" }))
 
-const [first, ...last] = ['Victor', 'Soares', 'Teste']
-
-
 const result = dataSeparated.map((nome) => {
-    return { nome } + "\n"
+    const [first, ...last] = nome.split(' ')
+    return {
+        nome: first,
+        sobrenome: last.join(' ')
+    }
 })
+
+result.shift();
+
+
+fs.writeFileSync( result2Path, JSON.stringify(result) )
+
 
 // Transform a variavel csvText em um objeto com atributo nome { nome: "Linha do CSV" }
 // Salve os dados transformados no arquivo result.json
